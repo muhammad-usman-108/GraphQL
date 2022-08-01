09f080c833bdf948e11a9c195917dc4ca8709751
muhammad-usman-108 <umuhammad202@yahoo.com>
2022-08-01 21:46:00 +0200

    GraphQL: Client & Server

---
+++ hello-world/client/app.js
@@ -0,0 +1,26 @@
+const GRAPH_URL = 'http://localhost:9000/'
+
+async function fetchGreeting() {
+    const response = await fetch(GRAPH_URL, {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({
+            query: `
+                query {
+                    greeting
+                }
+            `
+        })
+    });
+    const { data } = await response.json();
+    return data;
+}
+
+const element = document.getElementById('greeting');
+element.textContent = 'Loading....';
+
+fetchGreeting().then(({ greeting }) => {
+    element.textContent = greeting;
+});
